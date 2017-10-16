3c0694942d1f6eb1bee79a21cb1c707f0eca5a14
Rodrigo Silva dos Santos <rodrigos.santos01@gmail.com>
2017-10-16 21:26:37 -0200

    Changed to mediator live data

--- app/src/main/java/com/combah/dindin2/room/RoomMovementRepository.kt
+++ app/src/main/java/com/combah/dindin2/room/RoomMovementRepository.kt
@@ -1,7 +1,7 @@
 package com.combah.dindin2.room
 
 import android.arch.lifecycle.LiveData
-import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.MediatorLiveData
 import android.arch.lifecycle.Observer
 import com.combah.dindin2.data.Movement
 import com.combah.dindin2.repository.MovementRepository
@@ -11,7 +11,7 @@ import com.combah.dindin2.room.entities.RoomMovement
 class RoomMovementRepository(private val movementDao: MovementDao) : MovementRepository {
 
     override fun movementsInPeriod(start: Long, end: Long): LiveData<List<Movement>> {
-        return movementDao.movementsInPeriod(start, end).asMovementListLiveData()
+        return movementDao.movementsInPeriod(start, end).toMovementListLiveData()
     }
 
     override fun incomeInPeriod(start: Long, end: Long): LiveData<Double> {
@@ -23,30 +23,36 @@ class RoomMovementRepository(private val movementDao: MovementDao) : MovementRep
     }
 
     override fun totalInPeriod(start: Long, end: Long): LiveData<Double> {
-        val totalLiveData = MutableLiveData<Double>()
+        val totalLiveData = MediatorLiveData<Double>()
         val income = incomeInPeriod(start, end)
         val expense = expenseInPeriod(start, end)
+        totalLiveData.value = income.value?.minus(expense.value ?: 0.0)
         val observer = Observer<Double> {
             totalLiveData.value = income.value?.minus(expense.value ?: 0.0)
         }
-        income.observeForever(observer)
-        expense.observeForever(observer)
+        totalLiveData.addSource(income, observer)
+        totalLiveData.addSource(expense, observer)
         return totalLiveData
     }
 
-    private fun LiveData<List<RoomMovement>>.asMovementListLiveData(): LiveData<List<Movement>> {
-        val result = MutableLiveData<List<Movement>>()
-        observeForever {
-            result.value = it?.map { roomMovement ->
-                Movement(
-                        id = roomMovement.id,
-                        date = roomMovement.date,
-                        income = roomMovement.income,
-                        description = roomMovement.description,
-                        value = roomMovement.value
-                )
-            }
+    private fun LiveData<List<RoomMovement>>.toMovementListLiveData(): LiveData<List<Movement>> {
+        val result = MediatorLiveData<List<Movement>>()
+        result.value = value?.toMovementList()
+        result.addSource(this) {
+            result.value = it?.toMovementList()
         }
         return result
+    }
+
+    private fun List<RoomMovement>.toMovementList(): List<Movement> {
+        return map { roomMovement ->
+            Movement(
+                    id = roomMovement.id,
+                    date = roomMovement.date,
+                    income = roomMovement.income,
+                    description = roomMovement.description,
+                    value = roomMovement.value
+            )
+        }
     }
 }
